59fb1bbb293edc9a99ba18941dbccb2de42cc03c
William <278976237@qq.com>
2018-07-23 09:55:02 +0800

    update code

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,13 @@
 import random
+import re
+from fuzzywuzzy import fuzz
 
 import torch
 import torch.nn as nn
 
 import load_data
 import modelNet
-from fuzzywuzzy import fuzz
 
-
-# device = torch.device("cuda:0")
 
 # 把句子转为tensor
 def tensorFromSentence(sentence, embedding):
@@ -54,6 +53,8 @@ def tensorsFromPair_verify(pair, embedding):
 def load_training_and_verify_pairs(pairs):
     pairs_true = []
     pairs_false = []
+
+    # 分开正负样本
     for pair in pairs:
         if pair[4] == '1':
             pairs_true.append(pair)
@@ -61,13 +62,21 @@ def load_training_and_verify_pairs(pairs):
             pairs_false.append(pair)
     random.shuffle(pairs_true)
     random.shuffle(pairs_false)
+    print('<size>===total pairs: {}, true pairs: {}, false pairs: {}'.format(
+        len(pairs_true) + len(pairs_false), len(pairs_true), len(pairs_false)))
+
+    # 按比例取训练集和验证集
     training_pairs = pairs_true[0:int(len(pairs_true) * modelNet.TRAINTEST_RATE)] + \
                      pairs_false[0:int(len(pairs_false) * modelNet.TRAINTEST_RATE)]
-    test_pairs = pairs_true[int(len(pairs_true) * modelNet.TRAINTEST_RATE):] + \
-                 pairs_false[int(len(pairs_false) * modelNet.TRAINTEST_RATE):]
+    verify_pairs = pairs_true[int(len(pairs_true) * modelNet.TRAINTEST_RATE):] + \
+                   pairs_false[int(len(pairs_false) * modelNet.TRAINTEST_RATE):]
+
+    print('<size>===training pairs: {}, verify pairs: {}'.format(
+        len(training_pairs), len(verify_pairs)))
+
     random.shuffle(training_pairs)
-    random.shuffle(test_pairs)
-    return training_pairs, test_pairs
+    random.shuffle(verify_pairs)
+    return training_pairs, verify_pairs
 
 
 # 部分抽取英语原语训练数据
@@ -98,14 +107,39 @@ def get_some_english_train_pairs(english_pairs, spanish_pairs):
     random.shuffle(english_pairs_false)
 
     # 按英语原语与西班牙原语的比例取英语原语正负样本
-    train_pairs = english_pairs_true[0:len(spanish_pairs_true) * modelNet.ENGLISH_SPANISH_RATE] + \
-                  english_pairs_false[0:len(spanish_pairs_false) * modelNet.ENGLISH_SPANISH_RATE]
+    train_pairs = english_pairs_true[0:int(len(spanish_pairs_true) * modelNet.ENGLISH_SPANISH_RATE)] + \
+                  english_pairs_false[0:int(len(spanish_pairs_false) * modelNet.ENGLISH_SPANISH_RATE)]
     return train_pairs
 
 
-# TODO: 取部分英语原语数据作为验证集
-def get_some_english_verify_pairs(english_pairs):
-    pass
+# 找到所有缺失的词
+def locate_missing_word(parameter):
+    word_vocab = load_data.loadVocab('preprocess/word_vocab.txt')
+    print('len of vocab: ', len(word_vocab))
+    print('len of dict: ', len(parameter.word_to_embedding))
+    print('missing: ', len(word_vocab) - len(parameter.word_to_embedding))
+
+    missing_char_vocab = []
+    missing_digit_vocab = []
+    with open('preprocess/all_missing_word.txt', encoding='utf-8', mode='wt') as file:
+        for word in word_vocab:
+            if word not in parameter.word_to_embedding.keys():
+                file.write(word + '\n')
+                res = re.search(r'\d', word)
+                if bool(res):
+                    missing_digit_vocab.append(word)
+                else:
+                    missing_char_vocab.append(word)
+
+    with open('preprocess/missing_digit_word.txt', encoding='utf-8', mode='wt') as file:
+        print('len of missing digit: ', len(missing_digit_vocab))
+        for word in missing_digit_vocab:
+            file.write(word + '\n')
+
+    with open('preprocess/missing_char_word.txt', encoding='utf-8', mode='wt') as file:
+        print('len of missing char: ', len(missing_char_vocab))
+        for word in missing_char_vocab:
+            file.write(word + '\n')
 
 
 # 将缺失的含有数字的词统一Embedding
@@ -127,48 +161,44 @@ def embedding_missing_digit_word():
     load_data.saveEmbedVocab(missing_digit_embedding, 'preprocess/missing_digit_embedding.txt')
 
 
+# 从词库加载所有词转成字典，并储存到本地，字典形式：{'word':index}
+def load_and_save_all_char_vocab():
+    all_char_vocab = {}
+    with open('data/wiki.es.vec', encoding='utf-8') as all_data:
+        for idx, line in enumerate(all_data):
+            if idx is 0:
+                continue  # 跳过首行
+            items = line.strip().split()
+            all_char_vocab[load_data.normalizeString(items[0])] = idx
+
+        load_data.saveVocab(all_char_vocab, 'preprocess/database_all_word_vocab.txt')
+        print('Save All vocab completed.')
+
+
 # 计算缺失的全是字母的词与词库中最近的词
 def embedding_missing_char_word():
     missing_char_vocab = load_data.loadVocab('preprocess/missing_char_word.txt')
     all_char_vocab = load_data.loadVocab('preprocess/database_all_word_vocab.txt')
-    # all_char_vocab = {}
     all_char_list = []
     for word in all_char_vocab:
         all_char_list.append(word)
 
-    # 从词库中加载所有词
-    # with open('data/wiki.es.vec', encoding='utf-8') as all_data:
-    #     for idx, line in enumerate(all_data):
-    #         if idx is 0:
-    #             continue    # 跳过首行
-    #         items = line.strip().split()
-    #         all_char_vocab[load_data.normalizeString(items[0])] = idx
-    #
-    #     load_data.saveVocab(all_char_vocab,'preprocess/database_all_word_vocab.txt')
-    #     print('Save All vocab completed.')
-
     # 计算缺失词最相近的词
-    # with open('preprocess/sim_word.txt', encoding='utf-8', mode='wt') as file:
-    #     for i, word in enumerate(missing_char_vocab):
-    #         # print('current word: %s' % word)
-    #         sim_data = []
-    #         DIFF_WEIGHT = 2  # 长度对相似度的影响程度
-    #         for idx, item in enumerate(all_char_list):
-    #             value = fuzz.partial_ratio(word, item)
-    #             len_diff = abs(len(word) - len(item))
-    #             sim_value = value - DIFF_WEIGHT * len_diff  # 长度差距越大，相似值越小
-    #             sim_data.append(sim_value)
-    #         sim_idx = sim_data.index(max(sim_data))
-    #         max_value = max(sim_data)
-    #         sim_word = all_char_list[sim_idx]
-    #         print('word: {}, sim word: {}, sim_idx: {}, sim value: {}'.format(
-    #             word, sim_word, sim_idx, max_value))
-    #         file.write(word + ' ' + sim_word + '\n')
-
-    # res1 = fuzz.ratio(w1, w2)
-    # res2 = fuzz.partial_ratio(w1, w2)
-    # res3 = fuzz.token_sort_ratio(w1, w2)
-    # res4 = fuzz.token_set_ratio(w1, w2)
+    with open('preprocess/sim_word.txt', encoding='utf-8', mode='wt') as file:
+        for i, word in enumerate(missing_char_vocab):
+            sim_data = []
+            DIFF_WEIGHT = 2  # 长度对相似度的影响程度
+            for idx, item in enumerate(all_char_list):
+                value = fuzz.partial_ratio(word, item)
+                len_diff = abs(len(word) - len(item))
+                sim_value = value - DIFF_WEIGHT * len_diff  # 长度差距越大，相似值越小
+                sim_data.append(sim_value)
+            sim_idx = sim_data.index(max(sim_data))
+            max_value = max(sim_data)
+            sim_word = all_char_list[sim_idx]
+            print('word: {}, sim word: {}, sim_idx: {}, sim value: {}'.format(
+                word, sim_word, sim_idx, max_value))
+            file.write(word + ' ' + sim_word + '\n')
 
 
 # 根据找到的相似词取词库中的词向量
@@ -196,7 +226,6 @@ def get_sim_word_embedding():
     load_data.saveEmbedVocab(sim_word_embedding, 'preprocess/sim_word_embedding.txt')
 
 
-# TODO: 合并所有的词向量，返回所有出现词的词向量
 def get_final_word_to_embedding():
     word_to_embedding = load_data.loadEmbedVocab('preprocess/word_embedding.txt')
 
