3fb61b961794924e4281164ddda4d2b05f7e27c1
William <278976237@qq.com>
2018-07-25 10:16:15 +0800

    update code

--- train.py
+++ train.py
@@ -145,3 +145,5 @@ class Instructor:
                     f.write("epoch " + str(i) + "  loss:" + str(restore_data['epoch' + str(i) + 'loss']) + "\n")
 
                 f.write("test loss:" + str(final_avg_loss) + "\n")
+        print('=' * 100)
+        print('Finished!')
