1528abfda17444d66e642327519c9e5e3f16cd0c
William <278976237@qq.com>
2018-07-25 13:49:50 +0800

    update code

--- main.py
+++ main.py
@@ -60,10 +60,10 @@ if __name__ == '__main__':
     instructor = Instructor(opt)
 
     # 显示训练前的结果
-    # instructor.beforeTrain()
+    instructor.beforeTrain()
 
     # 开始训练模型
-    # instructor.beginTrain()
+    instructor.beginTrain()
 
     # 显示训练后在验证集上的结果
     instructor.verifyModel()
