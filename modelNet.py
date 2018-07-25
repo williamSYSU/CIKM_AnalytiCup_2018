4239863ba34a8eea8759b1089a0270314e31b30d
monsterspeaking <1365973991@qq.com>
2018-07-25 14:55:29 +0800

    update code

--- modelNet.py
+++ modelNet.py
@@ -204,7 +204,7 @@ class MatchSRNN(nn.Module):
                         s_ij = self.getS(input1[t][i], input2[t][j])
                         s = torch.cat((s, s_ij), dim=0)
             s = s.view(input1[t].size(0), input2[t].size(0), -1)
-            # print(s)
+            print("s:", s)
             all_hidden = [[] for i in range(input1[t].size(0))]
             for i in range(input1[t].size(0)):
                 for j in range(input2[t].size(0)):
@@ -217,7 +217,7 @@ class MatchSRNN(nn.Module):
                 out = batch_all_hidden[t][-1][-1].unsqueeze(0)
             else:
                 out = torch.cat((out, batch_all_hidden[t][-1][-1].unsqueeze(0)), dim=0)
-        # print(out)
+        print("out:", out)
         # print("ba:",batch_all_hidden[:][input1[t].size(0) - 1][input2[t].size(0) - 1])
         out = self.lastlinear(out)
         out = F.softmax(out, dim=1)
